0ac8d585965491ef433a7c85f5fc5ab2d6e882bd
francesco <f.iacono89@gmail.com>
2021-01-22 17:32:58 +0100

    secondo commit

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
-$discs = [
-  [
-    "poster" => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
-    "title" => "New Jersey",
-    "author" => "Bon Jovi",
-    "genre" => "Rock",
-    "year" => "1988"
-  ],
-  [
-    "poster" => "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
-    "title" => "Live at Wembley 86",
-    "author" => "Queen",
-    "genre" => "Pop",
-    "year" => "1992"
-  ],
-],
+include __DIR__ . '/db.php';
 
  ?>
 <!DOCTYPE html>
@@ -29,8 +14,10 @@ $discs = [
 </head>
 <body>
   <main>
+    <?php foreach ($discs as $key => $disc): ?>
+      <h1><?php echo $disc["title"]?></h1>
+    <?php endforeach; ?>
 
   </main>
-
 </body>
 </html>
