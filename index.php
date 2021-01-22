fc4b5cce6acad8bd5e37ff3207f2e8e0c08e6ab9
francesco <f.iacono89@gmail.com>
2021-01-22 17:15:20 +0100

    primo commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$discs = [
+  [
+    "poster" => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+    "title" => "New Jersey",
+    "author" => "Bon Jovi",
+    "genre" => "Rock",
+    "year" => "1988"
+  ],
+  [
+    "poster" => "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
+    "title" => "Live at Wembley 86",
+    "author" => "Queen",
+    "genre" => "Pop",
+    "year" => "1992"
+  ],
+],
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- foglio di stile -->
+  <link rel="stylesheet" href="css/app.css">
+  <title>Dischi</title>
+</head>
+<body>
+  <main>
+
+  </main>
+
+</body>
+</html>
